b430e38a361c83dd0b49c1ffc80bf593b839f31b
Juan carlos salcedo <juank9225@gmail.com>
2021-05-26 12:32:22 -0500

    Se crea la clase pizarrón (Board)  y se modifican los prototipos de esta para agregar método a ella.

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-alert("hola mundo .... tengo conexion");
+(function(){
+        self.Board = function(width,height) {
+        this.width = width;
+        this.height = height;
+        this.playing = false;
+        this.game_over = false;
+        this.bars = [];
+        this.ball = null;
+    };
+
+    self.Board.prototype = {
+
+        get elements(){
+            var elements = this.bars;
+            elements.push(this.ball);
+            return elements;
+        },
+    };
+})();
+
+function main(){
+
+}
