c4c2dfdf1dca52ad8590e6a824579038eb8b6886
Juan carlos salcedo <juank9225@gmail.com>
2021-05-26 13:28:55 -0500

    implantamos un método para pintar la vista del juego con canvas (BoardView).

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MvcPracticaJavaScript
 desarrollar un juego de ping-pong utilizando java Script como lenguaje de programación.
+preguntas:
+---------
+
+*como crear funciones anonimas en JavaScript?.
+*como crear clases por medio de prototype?:
+
--- main.js
+++ main.js
@@ -18,6 +18,22 @@
     };
 })();
 
+(function(){
+    self.BoardView = function(canvas,board){
+        this.canvas=canvas;
+        this.canvas.width = board.width;
+        this.canvas.height=board.height;
+        this.board = board;
+        this.contexto = canvas.getContext("2d");
+
+    };
+})();
+
+window.addEventListener("load",main);
 function main(){
+    var board = new Board(800,400);
+    var canvas =document.getElementById("canvas");
+    var board_view = new BoardView(canvas,board);
+    
 
 }
